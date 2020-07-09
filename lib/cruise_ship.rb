75c7c78138ac0e96affdc97bac6d9183843706e5
Cristian Deleon <libaration@gmail.com>
2020-07-09 21:02:50 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
+ #suite_a: "Amanda Presley",
+ #suite_b: "Seymour Hoffman",
+ #suite_c: "Alfred Tennyson",
+ #suite_d: "Charlie Chaplin",
+ #suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  ##must be staying in Suite and their name must start with the letter "A".
+  passengers.collect do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      return name
+    end
+  end
+
 end
